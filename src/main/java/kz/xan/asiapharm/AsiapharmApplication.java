c5c79ee2e1e869788b3a3f09257e2dc61b794bfb
XANALI <53555116+XANALI@users.noreply.github.com>
2020-07-02 16:27:17 +0600

    Project initialisation.

---
+++ src/main/java/kz/xan/asiapharm/AsiapharmApplication.java
@@ -0,0 +1,13 @@
+package kz.xan.asiapharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiapharmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsiapharmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/xan/asiapharm/AsiapharmApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.xan.asiapharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsiapharmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
