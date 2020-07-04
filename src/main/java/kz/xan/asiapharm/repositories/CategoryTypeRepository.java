9c3e2b20f63b50d4a563564b550ad19a992645e3
XANALI <53555116+XANALI@users.noreply.github.com>
2020-07-04 21:04:45 +0600

    Added CategoryType JPA Repository. closes #20

---
+++ src/main/java/kz/xan/asiapharm/repositories/CategoryTypeRepository.java
@@ -0,0 +1,7 @@
+package kz.xan.asiapharm.repositories;
+
+import kz.xan.asiapharm.domain.CategoryType;
+import kz.xan.asiapharm.services.CrudService;
+
+public interface CategoryTypeRepository extends CrudService<CategoryType, Long> {
+}
