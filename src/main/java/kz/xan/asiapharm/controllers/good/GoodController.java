8cf6028770a8980a94cea78ceecebf9d9a63eb97
XANALI <53555116+XANALI@users.noreply.github.com>
2020-07-04 15:11:16 +0600

    Add Good Controller with listUsers method. closes #11. closes #12.

---
+++ src/main/java/kz/xan/asiapharm/controllers/good/GoodController.java
@@ -0,0 +1,23 @@
+package kz.xan.asiapharm.controllers.good;
+
+import kz.xan.asiapharm.services.GoodService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/good")
+@Controller
+public class GoodController {
+    private final GoodService goodService;
+
+    public GoodController(GoodService goodService) {
+        this.goodService = goodService;
+    }
+
+    @RequestMapping("/show-all")
+    public String listGoods(Model model){
+        model.addAttribute("goods", goodService.findAll());
+
+        return "goods";
+    }
+}
