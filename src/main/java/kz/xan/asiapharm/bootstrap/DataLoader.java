bdba44557b8d3550a5c72998ebdfc25c33a00b06
XANALI <53555116+XANALI@users.noreply.github.com>
2020-07-05 10:06:19 +0600

    Dataloader refactored. Added Entities and saved by EntityServices to the h2 database. closes #18

--- src/main/java/kz/xan/asiapharm/bootstrap/DataLoader.java
+++ src/main/java/kz/xan/asiapharm/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package kz.xan.asiapharm.bootstrap;
 
-import kz.xan.asiapharm.services.CategoryService;
-import kz.xan.asiapharm.services.GoodService;
-import kz.xan.asiapharm.services.OrderService;
-import kz.xan.asiapharm.services.UserService;
+import kz.xan.asiapharm.domain.*;
+import kz.xan.asiapharm.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -14,15 +14,57 @@ public class DataLoader implements CommandLineRunner {
     private final GoodService goodService;
     private final CategoryService categoryService;
     private final OrderService orderService;
+    private final CategoryTypeService categoryTypeService;
 
-    public DataLoader(UserService userService, GoodService goodService, CategoryService categoryService, OrderService orderService) {
+    public DataLoader(UserService userService, GoodService goodService, CategoryService categoryService, OrderService orderService, CategoryTypeService categoryTypeService) {
         this.userService = userService;
         this.goodService = goodService;
         this.categoryService = categoryService;
         this.orderService = orderService;
+        this.categoryTypeService = categoryTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        User user1 = new User();
+        user1.setFirstName("Alikhan");
+        user1.setLastName("Ryskhan");
+        user1.setUsername("xan");
+        user1.setPassword("alikhan01");
+        user1.setBirthDate(LocalDate.now());
+        user1.setEmail("ryskhan.ali@gmail.com");
+        user1.setRole(ROLE.ADMIN);
+        user1.setMobileNum("87081843486");
+
+        userService.save(user1);
+
+        CategoryType categoryType = new CategoryType();
+        categoryType.setName("Лекарственные средства");
+        categoryType.setDescription("Medicines");
+
+        categoryTypeService.save(categoryType);
+
+        Category category = new Category();
+        category.setName("Жаропонижающие");
+        category.setDescription("smth");
+        category.setCategoryType(categoryType);
+
+        categoryService.save(category);
+
+        Good good = new Good();
+        good.setName("Nurofen");
+        good.setPrice(765);
+        good.setQuantity(20);
+        good.setDescription("smth");
+        good.setCategory(category);
+
+        goodService.save(good);
+
+        Order order = new Order();
+        order.setUser(user1);
+        order.setGood(good);
+        order.setDescription("smth");
+
+        orderService.save(order);
     }
 }
