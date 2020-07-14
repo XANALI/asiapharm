58e73ec14d605f8ce5c00190432396f768d0294e
XANALI <53555116+XANALI@users.noreply.github.com>
2020-07-14 14:21:47 +0600

    Implemented getting a category and editing. closes #47.

--- src/main/java/kz/xan/asiapharm/controllers/category/CategoryController.java
+++ src/main/java/kz/xan/asiapharm/controllers/category/CategoryController.java
@@ -1,8 +1,14 @@
 package kz.xan.asiapharm.controllers.category;
 
+import kz.xan.asiapharm.commands.CategoryCommand;
+import kz.xan.asiapharm.converters.CategoryCommandToCategory;
+import kz.xan.asiapharm.converters.CategoryToCategoryCommand;
 import kz.xan.asiapharm.services.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/category")
@@ -10,15 +16,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final CategoryToCategoryCommand toCommandconverter;
+    private final CategoryCommandToCategory fromCommandConverter;
 
-    public CategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService, CategoryToCategoryCommand toCommandconverter, CategoryCommandToCategory fromCommandConverter) {
         this.categoryService = categoryService;
+        this.toCommandconverter = toCommandconverter;
+        this.fromCommandConverter = fromCommandConverter;
     }
 
     @RequestMapping("/show-all")
     public String listCategories(Model model){
         model.addAttribute("categories", categoryService.findAll());
 
-        return "categories";
+        return "category/categories";
+    }
+
+    @RequestMapping("/{id}")
+    public String getCategory(@PathVariable Long id, Model model){
+        CategoryCommand categoryCommand = toCommandconverter.convert(categoryService.findById(id));
+        model.addAttribute("categoryCommand", categoryCommand);
+
+        return "category/singleInfo";
+    }
+
+    @PostMapping("/changeCategory")
+    public String changeCategory(@ModelAttribute CategoryCommand categoryCommand){
+        CategoryCommand savedCategoryCommand = categoryService.saveCategoryCommand(categoryCommand);
+
+        return "redirect:show-all";
     }
 }
