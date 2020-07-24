aad678899bb3f67f656b2d6e928484a793cf3ecd
XANALI <53555116+XANALI@users.noreply.github.com>
2020-07-24 23:41:36 +0600

    Added good adding, showing, editing methods and templates. closes #60.

--- src/main/java/kz/xan/asiapharm/controllers/good/GoodController.java
+++ src/main/java/kz/xan/asiapharm/controllers/good/GoodController.java
@@ -1,18 +1,25 @@
 package kz.xan.asiapharm.controllers.good;
 
+import kz.xan.asiapharm.commands.GoodCommand;
+import kz.xan.asiapharm.converters.GoodCommandToGood;
+import kz.xan.asiapharm.converters.GoodToGoodCommand;
+import kz.xan.asiapharm.domain.Good;
 import kz.xan.asiapharm.services.GoodService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/good")
 @Controller
 public class GoodController {
     private final GoodService goodService;
+    private final GoodToGoodCommand toGoodCommand;
+    private final GoodCommandToGood fromGoodCommand;
 
-    public GoodController(GoodService goodService) {
+    public GoodController(GoodService goodService, GoodToGoodCommand toGoodCommand, GoodCommandToGood fromGoodCommand) {
         this.goodService = goodService;
+        this.toGoodCommand = toGoodCommand;
+        this.fromGoodCommand = fromGoodCommand;
     }
 
     @RequestMapping("/show-all")
@@ -27,5 +34,47 @@ public class GoodController {
         goodService.deleteById(id);
 
         return "redirect:/good/show-all";
+    }
+
+    @GetMapping("/{id}/showInfo")
+    public String getGood(@PathVariable Long id, Model model){
+        model.addAttribute("goodCommand", toGoodCommand.convert(goodService.findById(id)));
+
+        return "good/singleInfo";
+    }
+
+    @GetMapping("/{id}/editInfo")
+    public String getEditGood(@PathVariable Long id, Model model){
+        model.addAttribute("goodCommand", toGoodCommand.convert(goodService.findById(id)));
+
+        return "good/editInfo";
+    }
+
+    @PostMapping("/{id}/editInfo")
+    public String postEditedGood(@PathVariable Long id, @ModelAttribute("goodCommand") GoodCommand goodCommand){
+        Good oldGood = goodService.findById(id);
+        Good editedGood = fromGoodCommand.convert(goodCommand);
+        if(oldGood != null){
+            assert editedGood != null;
+            editedGood.setImage(oldGood.getImage());
+        }
+        goodService.save(editedGood);
+
+        return "redirect:"+id+"/showInfo";
+    }
+
+    @GetMapping("/newInfo")
+    public String getNewGood(Model model){
+        model.addAttribute("goodCommand", new GoodCommand());
+
+        return "good/newInfo";
+    }
+
+    @PostMapping("/newInfo")
+    public String postNewGood(@ModelAttribute("goodCommand") GoodCommand goodCommand){
+        Good newGood = fromGoodCommand.convert(goodCommand);
+        Good savedGood = goodService.save(newGood);
+
+        return "redirect:"+savedGood.getId()+"/showInfo";
     }
 }
